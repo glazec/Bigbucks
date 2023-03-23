1d5cde55f807897b32c11c18f93a8219605f679a
Wenjie Cui <wc190@duke.edu>
2023-03-23 13:06:41 -0400

    Fix venv

--- BigBucks/flaskvenv/bin/activate.fish
+++ BigBucks/flaskvenv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cuiwenjie/Desktop/fintech512-bigbucks/BigBucks/flaskvenv"
+set -gx VIRTUAL_ENV "/Users/cuiwenjie/Desktop/fintech512-bigbucks/fintech512-bigbucks/BigBucks/flaskvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
